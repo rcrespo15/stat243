bad5602dde0359d87a602f9a0f7b3779afdd728a
Other <other@RamonC.local>
2018-11-02 00:55:56 -0700

    added bash files to run rscript

---
+++ ps6/script.R
@@ -0,0 +1,21 @@
+library(readr)
+library(parallel)
+library(foreach)
+
+df <- data.frame()
+results <- foreach(i = 0:959)%dopar%{
+  directory <- ("/global/scratch/paciorek/wikistats_full/dated_for_R/")
+  if (i < 10){
+    filename <- paste("part-0000",as.character(i),sep="")
+  } else if (i < 100){
+    filename <- paste("part-000",as.character(i),sep="")
+  } else{
+    filename <- paste("part-00",as.character(i),sep="")
+  }
+  file_name <- paste(directory,filename,sep="")
+  data = read_delim(file_name, delim = " ")
+  data$barack <- data[4] == "Barack_Obama"
+  z <- subset(data, barack == TRUE)
+  df <- rbind(df,z)
+}
+save(df,file="data.Rda")
